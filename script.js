7c9ffd68460761491d67613af8f22f9e26864665
BelgicaGly <84245740+BelgicaGly@users.noreply.github.com>
2021-05-17 01:44:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,149 @@
+function myFview() {
+  var dots = document.getElementById("dotsview");
+  var moreText = document.getElementById("moreview");
+  var btnText = document.getElementById("myBview");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+function myFun() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunction() {
+  var dots = document.getElementById("dot");
+  var moreText = document.getElementById("mo");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunc() {
+  var dots = document.getElementById("dot1");
+  var moreText = document.getElementById("mor");
+  var btnText = document.getElementById("myGln");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFunc21(){
+  var dots = document.getElementById("dot21");
+  var moreText = document.getElementById("mor21");
+  var btnText = document.getElementById("myGln21");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFufu(){
+  var dots = document.getElementById("dotdot");
+  var moreText = document.getElementById("mormor");
+  var btnText = document.getElementById("myGlgi");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+function myFu99(){
+  var dots = document.getElementById("dot99");
+  var moreText = document.getElementById("more99");
+  var btnText = document.getElementById("myGlgi21");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
+
+
+
+$(document).ready(function(){
+
+  $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
+  });
+
+  $(window).on('scroll load',function(){
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+  });
+
+  $('.amount input').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+  });
+
+  // smooth scrolling 
+
+  $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500,	
+      'linear'
+    );
+
+  });
+
+});
